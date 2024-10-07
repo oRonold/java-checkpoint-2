26fbcb14e97880c36af4fca88c146ed5df064017
oRonold <or040918@gmail.com>
2024-10-07 16:10:33 -0300

    Refactoring some entities

--- src/main/java/br/com/fiap/checkpoint/dois/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.checkpoint.dois.repository;
-
-import br.com.fiap.checkpoint.dois.model.subject.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
